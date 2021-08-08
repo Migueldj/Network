5d304533aa5b55cd736c4d2aa210d6d8904573db
Miguel Angel De Jesús <migueldj15@gmail.com>
2021-08-07 22:50:19 -0500

    Post model created and User model modified, beta version

--- network/models.py
+++ network/models.py
@@ -1,6 +1,17 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 
 class User(AbstractUser):
-    pass
+    followers = models.ManyToManyField("self", blank=True)
+    following = models.ManyToManyField("self", blank=True)
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=CASCADE, related_name="user_posts")
+    content = models.CharField(max_length = 100)
+    likes = models.ManyToManyField(User, blank=True)
+
+
+
+
